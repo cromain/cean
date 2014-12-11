5f560b4989f2a5fbb0cc4cc155a87c9aa4b99886
Christophe Romain <christophe.romain@process-one.net>
2014-12-11 15:38:54 +0100

    minor improvements in ejabberd pakcaging

--- pkg/ejabberd/prep_sources.sh
+++ pkg/ejabberd/prep_sources.sh
@@ -6,7 +6,7 @@ rm doc/release_notes*
 # generate configure
 ./autogen.sh
 mv configure configure.old
-sed "s/community/$2/" configure.old > configure
+sed "s/0\.0/$2/" configure.old > configure
 chmod +x configure
 rm configure.old
 
@@ -16,3 +16,6 @@ echo "vsn,\"$2\"" > ebin/ejabberd.app
 (cd doc; rm guide.html; make)
 (cd doc; rm guide.aux guide.haux guide.htoc guide.idx guide.ilg guide.ind guide.log guide.out guide.toc)
 rm -Rf ebin
+
+# copy README
+cp README doc/README.txt
