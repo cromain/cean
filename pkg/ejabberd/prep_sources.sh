25b66aae75c1af34568a3eaedd46f63c178142f4
Christophe Romain <christophe.romain@process-one.net>
2014-12-12 12:20:08 +0100

    fix ejabberd version and include static versions of deps

--- pkg/ejabberd/prep_sources.sh
+++ pkg/ejabberd/prep_sources.sh
@@ -4,11 +4,10 @@
 rm doc/release_notes*
 
 # generate configure
-./autogen.sh
-mv configure configure.old
-sed "s/0\.0/$2/" configure.old > configure
+vsn=${2//-/.}
+aclocal -I m4
+autoconf -f -o - | sed "s/0\.0/$vsn/" > configure
 chmod +x configure
-rm configure.old
 
 # generate guide
 mkdir ebin
@@ -16,6 +15,11 @@ echo "vsn,\"$2\"" > ebin/ejabberd.app
 (cd doc; rm guide.html; make)
 (cd doc; rm guide.aux guide.haux guide.htoc guide.idx guide.ilg guide.ind guide.log guide.out guide.toc)
 rm -Rf ebin
+
+# generate static deps versions
+./rebar get-deps
+tools/set-dep-versions
+rm -Rf deps
 
 # copy README
 cp README doc/README.txt
