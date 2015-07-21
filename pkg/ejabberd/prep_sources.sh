17a383722ede80ad6e92428a7f3aaa289e74481d
Christophe Romain <christophe.romain@process-one.net>
2015-07-21 16:12:14 +0200

    Hardcode ejabberd version on source tarball

--- pkg/ejabberd/prep_sources.sh
+++ pkg/ejabberd/prep_sources.sh
@@ -2,7 +2,7 @@
 
 # generate configure
 vsn=${2//-/.}
-sed -i -e "s/echo 0\.0/echo $vsn/" configure.ac
+sed -i -e "/AC_INIT/s/AC_INIT(.*/AC_INIT(ejabberd, $vsn, ejabberd@process-one.net, ejabberd)/" configure.ac
 ./autogen.sh
 
 # generate static deps versions
