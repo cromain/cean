214d28b2abda3d2d027877e7397738a8d11bf770
Christophe Romain <christophe.romain@process-one.net>
2015-08-11 18:01:48 +0200

    Enforce package sources generation

--- pkg/ejabberd/prep_sources.sh
+++ pkg/ejabberd/prep_sources.sh
@@ -4,6 +4,7 @@
 vsn=${2//-/.}
 sed -i -e "/AC_INIT/s/AC_INIT(.*/AC_INIT(ejabberd, $vsn, ejabberd@process-one.net, ejabberd)/" configure.ac
 ./autogen.sh
+./configure $(sed -e '/{configure,/!d;s/.*"\(.*\)".*/\1/' $3)
 
 # generate static deps versions
 ./rebar get-deps
