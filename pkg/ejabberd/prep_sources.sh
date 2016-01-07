7f59b49425e7343a23cd55e276c9b538272f1b21
Christophe Romain <christophe.romain@process-one.net>
2016-01-07 12:44:56 +0100

    Apply new ejabberd packaging

--- pkg/ejabberd/prep_sources.sh
+++ pkg/ejabberd/prep_sources.sh
@@ -4,9 +4,7 @@
 vsn=${2//-/.}
 sed -i -e "/AC_INIT/s/AC_INIT(.*/AC_INIT(ejabberd, $vsn, ejabberd@process-one.net, ejabberd)/" configure.ac
 ./autogen.sh
-./configure $(sed -e '/{configure,/!d;s/.*"\(.*\)".*/\1/' $3)
 
-# generate static deps versions
-./rebar get-deps
-tools/set-dep-versions
-rm -Rf deps
+# fetch deps
+#./configure $(sed -e '/{configure,/!d;s/.*"\(.*\)".*/\1/' $3)
+#./rebar get-deps
